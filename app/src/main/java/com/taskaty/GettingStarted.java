068d454a101b293ea1beb850d5ac2abe6dbd1d5e
Sondos <sondosahmedabed@gmail.com>
2023-11-10 18:16:33 +0200

    Add App Launcher

---
+++ app/src/main/java/com/taskaty/GettingStarted.java
@@ -0,0 +1,43 @@
+package com.taskaty;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/*
+    This is the starting activity of the mobile app, the user is able to
+    start
+ */
+public class GettingStarted extends AppCompatActivity {
+    Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initialize();
+    }
+
+    private void initialize() {
+        getSupportActionBar().hide();
+        setContentView(R.layout.gettingstarted);
+        setStart(findViewById(R.id.start));
+        handle_start(getStart());
+    }
+
+    private void handle_start(Button start) {
+        start.setOnClickListener(view -> {
+
+        });
+    }
+
+    /*
+        Getters & Setters
+     */
+    public Button getStart() {
+        return start;
+    }
+
+    public void setStart(Button start) {
+        this.start = start;
+    }
+}
---
+++ app/src/main/java/com/taskaty/Task.java
@@ -0,0 +1,12 @@
+package com.taskaty;
+
+/*
+    This is a class that I created to represent the task object
+    and it's attribure
+ */
+public class Task {
+    /*
+        Attributes
+     */
+
+}
