b8e284b4a20d1c6ccb10cb957f889c9bc23b591d
Sondos <sondosahmedabed@gmail.com>
2023-11-10 19:21:48 +0200

    Model: Create 'Task Model'

--- app/src/main/java/com/taskaty/Task.java
+++ app/src/main/java/com/taskaty/Task.java
@@ -1,5 +1,7 @@
 package com.taskaty;
 
+import java.util.Date;
+
 /*
     This is a class that I created to represent the task object
     and it's attribure
@@ -8,5 +10,73 @@ public class Task {
     /*
         Attributes
      */
+    private String title; // The title of the task ex: Shopping For Mom
+    private Boolean isDone = false; // If true then it is done, if false then it's to do
+    private String description; // A description of the task I will make it optional
+    private String category; // Personal, Work, Shopping, to-read, to-watch
+    private Date dueDate; // They can set the due date of the task
 
+    /*
+        Constructors
+     */
+    public Task(String title, Date dueDate) {
+        this.title = title;
+        this.dueDate = dueDate;
+    }
+
+    public Task(String title, String category, Date dueDate) {
+        this.title = title;
+        this.category = category;
+        this.dueDate = dueDate;
+    }
+
+    public Task(String title, String description, String category, Date dueDate) {
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.dueDate = dueDate;
+    }
+
+    /*
+        Getters & Setters
+    */
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getDone() {
+        return isDone;
+    }
+
+    public void setDone(Boolean done) {
+        isDone = done;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date date) {
+        this.dueDate = date;
+    }
 }
