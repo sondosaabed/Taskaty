c691e4c66525a1bd1e778613f19b550e7783758d
Sondos <sondosahmedabed@gmail.com>
2023-11-13 17:59:35 +0200

    Add Home activity that shows a list of tasks, and ability to add

--- app/src/main/java/com/taskaty/Home.java
+++ app/src/main/java/com/taskaty/Home.java
@@ -1,7 +1,10 @@
 package com.taskaty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
     This is the main app activity
  */
 public class Home extends AppCompatActivity{
+    ImageButton add;
+    ListView tasks;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
@@ -18,5 +24,30 @@ public class Home extends AppCompatActivity{
 
     private void initialize() {
         setContentView(R.layout.home);
+        setAdd(findViewById(R.id.add));
+        setTasks(findViewById(R.id.taskListView));
+        handle_add(getAdd());
+    }
+
+    private void handle_add(ImageButton add) {
+        add.setOnClickListener(veiw->{
+            Intent intent = new Intent(this, AddNewTask.class);
+            startActivity(intent);
+        });
+    }
+
+    /*
+        Getters & Setters
+     */
+    public void setAdd(ImageButton add) {
+        this.add = add;
+    }
+
+    public ImageButton getAdd() {
+        return add;
+    }
+
+    public void setTasks(ListView tasks) {
+        this.tasks = tasks;
     }
 }
