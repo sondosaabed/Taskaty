c7254b7d25770feec06e5f27c0bbf9560aa31bd3
Sondos <sondosahmedabed@gmail.com>
2023-11-13 18:05:03 +0200

    Update on Home activity

--- app/src/main/java/com/taskaty/Home.java
+++ app/src/main/java/com/taskaty/Home.java
@@ -17,8 +17,8 @@ public class Home extends AppCompatActivity{
     ListView tasks;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         initialize();
     }
 
