f890881c6705850b84abffea8b45261776a1db9a
Sondos <sondosahmedabed@gmail.com>
2023-11-14 03:42:53 +0200

    Handle back to home buttons (CompletedTask.java, UpdatedTask.java)

--- app/src/main/java/com/taskaty/informational/CompletedTask.java
+++ app/src/main/java/com/taskaty/informational/CompletedTask.java
@@ -1,21 +1,41 @@
 package com.taskaty.informational;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import com.taskaty.R;
+import com.taskaty.taskManagment.Home;
 
 public class CompletedTask extends AppCompatActivity {
+    Button back;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initialize();
     }
 
     private void initialize() {
         setContentView(R.layout.completed);
+        setBack(findViewById(R.id.backHome));
+        handle_back(getBack());
+    }
+
+    private void handle_back(Button back) {
+        back.setOnClickListener(veiw ->{
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+        });
+    }
+
+    /*
+        Getters & Setters
+     */
+    public void setBack(Button back) {
+        this.back = back;
+    }
+
+    public Button getBack() {
+        return back;
     }
 }
--- app/src/main/java/com/taskaty/informational/UpdatedTask.java
+++ app/src/main/java/com/taskaty/informational/UpdatedTask.java
@@ -1,14 +1,14 @@
 package com.taskaty.informational;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import com.taskaty.R;
+import com.taskaty.taskManagment.Home;
 
 public class UpdatedTask extends AppCompatActivity {
+    Button back;
     @Override
     public void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +17,25 @@ public class UpdatedTask extends AppCompatActivity {
 
     private void initialize() {
         setContentView(R.layout.updated);
+        setBack(findViewById(R.id.backHome));
+        handle_back(getBack());
+    }
+
+    private void handle_back(Button back) {
+        back.setOnClickListener(veiw ->{
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+        });
+    }
+
+    /*
+        Getters & Setters
+     */
+    public void setBack(Button back) {
+        this.back = back;
+    }
+
+    public Button getBack() {
+        return back;
     }
 }
