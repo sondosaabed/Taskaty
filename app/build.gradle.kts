042cb35feb9aac2643b90dee7a78a8fe758aa07f
Sondos <sondosahmedabed@gmail.com>
2023-11-15 20:25:30 +0200

    Add gson:2.10.1 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
